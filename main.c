44655effb6be3de2f8a102a46c1aa0dd315e6524
Naitik Sahu <naiitik1526@gmail.com>
2026-09-15 14:13:50 -0300

    made add task , printtask and createTask functions

--- main.c
+++ main.c
@@ -1,4 +1,56 @@
 #include <stdio.h>
-int main{
-    
+#include <stdbool.h>
+#include <string.h>
+typedef struct{
+    int id;
+    char title[100];
+    int completed;
+
+}task;
+
+task createTask(int id, char title[], int completed){
+    task newTask;
+    newTask.id= id;
+    strcpy(newTask.title, title); // newTask.title =title would not work
+    newTask.completed=completed;
+
+    return newTask;
 }
+
+void printTask(task t){
+    printf("ID: %d\n", t.id);
+    printf("Title: %s\n",t.title);
+    if(t.completed ==0){
+        printf("completed: No\n");
+    }
+    else{
+        printf("completed: yes\n");
+    }
+}
+
+void addTask(task tasks[], int *size){
+    tasks[*size] = createTask(*size + 1 , "new task", 0);
+    (*size)++;
+
+}
+
+int main(){
+    task tasks[10];
+    int size =3;
+
+    tasks[0]= createTask(1, "code for 3 hours", true);
+    tasks[1]= createTask(2, "play valo", false);
+    tasks[2]= createTask(3, "larp", true);
+    addTask(tasks, &size);
+
+    for(int i=0; i<size; i++)
+    {
+        printTask(tasks[i]);
+    }
+    return 0;
+}
+    
+    
+
+
+
